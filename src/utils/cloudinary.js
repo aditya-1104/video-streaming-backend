a4e72b24c2879610577ff997528baeefe1c52ead
Aditya Ranjan <adityaranjan111104@gmail.com>
2025-06-29 15:32:21 +0530

    User's Update Controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { ApiError } from "./ApiError";
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -24,4 +25,22 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 };
 
-export { uploadOnCloudinary };
+const deleteOnCloudinary = async (imageUrl) => {
+
+    try {
+        const urlParts = imageUrl.split('/');
+        const fileWithExtension = urlParts[urlParts.length - 1]; 
+        const folder = urlParts[urlParts.length - 2]; 
+
+        const fileName = fileWithExtension.split('.')[0]; 
+        const publicId = `${folder}/${fileName}`;
+
+        const result = await cloudinary.uploader.destroy(publicId);
+    
+        return result;
+    } catch (err) {
+        throw new ApiError(500, err.message || "Error Deleting File on Cloudinary");
+  }
+};
+
+export { uploadOnCloudinary , deleteOnCloudinary };
